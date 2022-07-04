37a8931e6fc90b5f2170d1fc6cdfa6f1008a278d
Alper Eş <alperes13@hotmail.com>
2022-07-04 23:04:39 +0300

    Version 2

--- src/Merhaba.java
+++ src/Merhaba.java
@@ -2,6 +2,14 @@ package src;
 
 public class Merhaba {
     public static void main(String[] args) {
+
+	int a = 10;
+	int b = 20;
+	
         System.out.println("Merhaba");
+
+	System.out.println("Değişiklik 2");
+
+	System.out.println(a+b);
     }
 }
